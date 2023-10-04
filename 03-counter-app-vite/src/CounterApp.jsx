f8cbea93bc4c515299ad2df26fb5e3ef51329d63
Diegomez-dev <diegoagc@yahoo.es>
2023-10-04 18:45:52 -0500

    handleSubs y handleReset

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -2,24 +2,26 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-export const CounterApp = ({ value }) => {
-//usamos el argumento de setCounter para modificar el valor del counter, por convencion para los cambios se usa set
-//el argumento enviado a useState es el valor por defecto del counter
-//podemos iniciar el estado con el valor value que enviamos desde el main.jsx
-  const [ counter, setCounter ] = useState(value);
+//cuando hay un cambio en el estado se vuelve a ejecutar el componente
 
-  const handleAdd = () => {
-    setCounter( counter + 1 );
-  };
+export const CounterApp = ({ value }) => {
+  //usamos el argumento de setCounter para modificar el valor del counter, por convencion para los cambios se usa set
+  //el argumento enviado a useState es el valor por defecto del counter
+  //podemos iniciar el estado con el valor value que enviamos desde el main.jsx
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => setCounter(counter + 1);
+  const handleSubstract = () => setCounter(counter - 1);
+  const handleReset = () => setCounter(value);
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { counter } </h2> 
+      <h2> {counter} </h2>
 
-      <button onClick={ handleAdd }>
-        +1
-      </button>
+      <button onClick={ handleAdd }> +1 </button>
+      <button onClick={ handleSubstract }> -1 </button>
+      <button onClick={ handleReset }> Reset </button>
     </>
   );
 }
