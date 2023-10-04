a17f835a6636f8711184f6fd7807bc9a38879083
Diegomez-dev <diegoagc@yahoo.es>
2023-10-04 08:41:16 -0500

    prop types

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,22 +1,21 @@
-// las funcionen debeb ser declaradas por fuera del functional componenr para evitar que cree un nuevo espacio de memoria cada vez que se renderiza el componente
-const nombre = 'Jorge';
-const getResult = (a, b) => {
-  return a + b;
-};
+//para las proptupes primero hacemos la importacion de la libreria y la instalamos en consola con
+//yarn add prop-types
+//las proptypes nos definen el tipo de dato que recibira el prop
+import PropTypes from "prop-types";
 
 export const FirstApp = ( { title, subTitle} ) => {
 
-//las porperties o props son argumentos (en este caso propiedades) que reciben las functonal components, estos props son objetos y generalmente se desestructura.
-
   return (
-    // cuando quiero retornar más de un elemento, debo envolverlos en un fragmento usando <> y </>, que actuan como nodo padre
-    //una expresion (que no sea un objeto) de javascript se puede imprimir creando una variable y dentro del html, usamos llaves para mosrtar el valor de la variable
     <>
       <h1>{ title }</h1>
       <p> { subTitle }</p>
-      <h1>{ getResult(1, 2) }</h1>
     </>
 
     //podemos mostrar el resultado o retorno de una funcion
   );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.number.isRequired
 };
