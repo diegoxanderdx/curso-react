74ceac889999ac7867d023dabb6106952e6ecd8d
Diegomez-dev <diegoagc@yahoo.es>
2023-10-04 10:36:15 -0500

    eventos

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,19 @@
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
+
+  const handleAdd = () => {
+    console.log('+1');
+    value = 1000;
+  }
+
   return (
     <>
       <h1>Counter App</h1>
-      <p> {value}</p>
+      <p> {value} </p>
+      <button onClick={ handleAdd }>
+        +1
+      </button>
     </>
   );
 };
