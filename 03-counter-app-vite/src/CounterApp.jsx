14c76efea88c9ed97ee3e4f69f0d5fccdb524bbf
Diegomez-dev <diegoagc@yahoo.es>
2023-10-04 18:22:56 -0500

    useState hook

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,23 +1,28 @@
+//importamos useState (use significa que es un hook pero basicamente es una funcion)
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
+//usamos el argumento de setCounter para modificar el valor del counter, por convencion para los cambios se usa set
+//el argumento enviado a useState es el valor por defecto del counter
+  const [ counter, setCounter ] = useState(value);
 
   const handleAdd = () => {
-    console.log('+1');
-    value = 1000;
-  }
+    setCounter( counter + 1 );
+  };
 
   return (
     <>
-      <h1>Counter App</h1>
-      <p> {value} </p>
+      <h1>CounterApp</h1>
+      <h2> { counter } </h2> 
+
       <button onClick={ handleAdd }>
         +1
       </button>
     </>
   );
-};
+}
 
 CounterApp.propTypes = {
   value: PropTypes.number.isRequired,
-};
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={ 1984 } />
+        <CounterApp value={ 0 } />
     </React.StrictMode>
 );
         
