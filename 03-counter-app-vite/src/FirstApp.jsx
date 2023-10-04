30462c2d61c383e001fb1bd9673b362e5d2f34a0
Diegomez-dev <diegoagc@yahoo.es>
2023-10-03 22:00:10 -0500

    props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -4,13 +4,16 @@ const getResult = (a, b) => {
   return a + b;
 };
 
-export const FirstApp = () => {
+export const FirstApp = ( { title, subTitle} ) => {
+
+//las porperties o props son argumentos (en este caso propiedades) que reciben las functonal components, estos props son objetos y generalmente se desestructura.
+
   return (
     // cuando quiero retornar más de un elemento, debo envolverlos en un fragmento usando <> y </>, que actuan como nodo padre
     //una expresion (que no sea un objeto) de javascript se puede imprimir creando una variable y dentro del html, usamos llaves para mosrtar el valor de la variable
     <>
-      <h1>{nombre}</h1>
-      <p>My first React App</p>
+      <h1>{ title }</h1>
+      <p> { subTitle }</p>
       <h1>{ getResult(1, 2) }</h1>
     </>
 
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -6,8 +6,7 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-
-        <FirstApp />
+        <FirstApp title="Hola soy goku" subTitle={ 123 } /> {/* aca podemos agregar los valores de las propiedades que recibe el componente, el subtitle en este caso es un valor diferenta a string */}
     </React.StrictMode>
 );
         
