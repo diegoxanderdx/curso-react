b3c919d2466c06369ab0b4e86fa49a8745e2c348
Diegomez-dev <diegoagc@yahoo.es>
2023-10-19 22:55:47 -0500

    toBe expect

--- 03-counter-app-vite/test/demos.test.js
+++ 03-counter-app-vite/test/demos.test.js
@@ -1,5 +1,13 @@
-test('Esta prueba no debe de fallar', () => {
-    if (false) {
-        throw new Error('Error');
-    }
+describe('Pruebas en el archivo demos.test.js', () => {
+
+    test('Esta prueba no debe de fallar', () => {
+        //Inicialización
+        const mensaje = 'Hola Mundo';
+
+        //Estimulo
+        const mensaje2 = `Hola Mundo`;
+
+        //Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    })
 })
